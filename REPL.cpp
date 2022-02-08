6abaf551f0da3c556298f2edc58a6c7338eff4d0
joe <you@example.com>
2022-02-08 15:33:30 -0500

    Finish Bill Primer rough draft.

---
+++ REPL.cpp
@@ -0,0 +1,82 @@
+#include "./REPL.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+Generator::Generator(std::istream& INSTREAM, std::ostream& OUTSTREAM,Evaluator&  EVALUATOR)
+{
+    std::cout << "Construct\n\n";
+    
+    instream    = INSTREAM;
+    outstream   = OUTSTREAM;
+    Evaluator   = EVALUATOR;
+    
+    // Read in files.
+    ifstream in_file("welcome.txt", ifstream::in);
+    in_file >> welcome;
+    in_file.close();
+    
+    ifstream in_file("prompt.txt", ifstream::in);
+    in_file >> prompt;
+    in_file.close();
+    
+    ifstream in_file("help.txt", ifstream::in);
+    in_file >> help;
+    in_file.close();
+    
+    std::string user_input;
+    std::string result;
+}
+
+REPL::read()
+{
+	std::string text;
+    
+    std::cout << prompt << " ";
+    getline(std::cin, text);
+//     std::cout << text << '\n';
+    user_input = text;
+}
+
+REPL::evaluate()
+{
+	if(user_input == "help")
+    {
+        result = user_input;
+    }
+    else
+    {
+        result = Evaluator.setInput()   // placeholder
+    }
+}
+
+REPL::print()
+{
+	std::cout << result << '\n';
+}
+
+REPL::loop()
+{
+	// call read
+    // call evaluate
+    // call print
+}
+
+REPL::~REPL()
+{
+    std::cout << "\nDestruct\n";
+    
+    delete  instream;
+    delete  outstream;
+    delete  Evaluator;
+}
+
+// std::ostream& operator<<(std::ostream& os, const REPL& rhs)
+// {
+//     os  << "default precision (6): " << rhs.getProduct() << '\n'
+//         << "std::setprecision(10): " << std::setprecision(10) << rhs.getProduct() << '\n'
+//         << "max precision:         "
+//         << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
+//         << rhs.getProduct() << "\n";
+//     return os;
+// }
---
+++ REPL.h
@@ -0,0 +1,60 @@
+#ifndef REPL_H
+#define REPL_H
+
+
+class REPL
+{
+    private:
+        std::istream    instream;   // reference to input stream
+        std::ostream    outstream;  // reference to output stream
+        ???             Evaluator;  // reference to Evaluator
+        
+        std::string     welcome;    // Welcom Screen
+        std::string     prompt;     // repl prompt
+        std::string     help;       // help screen
+    
+    protected:
+        /* read
+         * @brief   Read user input.
+         */
+        void read();
+        
+        /* evaluate
+         * @brief   Evaluate the input.
+         */
+        void evaluate();
+        
+        /* print
+         * @brief   Print rhe output.
+         */
+        void print();
+        
+    public:
+        
+        /* constructor
+         * @brief   Set up repl.
+         * @details Store instream, outstream, and Evaluator references. Also store contents of welcome.txt, prompt.txt, and help.txt.
+         * @param   instream            (reference)
+         * @param   outstream           (reference)
+         * @param   Evaluator           (reference)
+         */
+        REPL(std::istream& INSTREAM, std::ostream& OUTSTREAM,Evaluator&  EVALUATOR);
+        REPL(const REPL& rhs);              // Copy Constructor
+        REPL(REPL&&) = delete;              // Move Constructor
+        REPL& operator=(const REPL& rhs);   // assignment operator
+        
+        /* loop
+         * @brief   Control the process.
+         * @note    This is the only method to call directly.
+         */
+        void loop();
+        
+        /* destructor
+         * @brief Tear Down REPL.
+         */
+        ~REPL();
+};
+
+// std::ostream& operator<<(std::ostream& os, const REPL& rhs);
+
+#endif
